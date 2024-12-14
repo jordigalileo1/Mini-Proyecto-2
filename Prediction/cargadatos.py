5d3d3e5f8c9b88ca56f19950facce0612f208c39
Jordi Chajon <jordi.chajon@galileo.edu>
2024-12-13 20:01:09 -0600

    version_final

--- Prediction/cargadatos.py
+++
@@ -1,9 +0,0 @@
-"""
-Este módulo se encarga de cargar y mostrar los datos del archivo CSV 'smartgrid.csv'.
-Utiliza la librería pandas para leer el archivo y mostrar las primeras filas del dataset.
-"""
-
-import pandas as pd
-
-datasmartgrid = pd.read_csv('../data/raw/smartgrid.csv')
-datasmartgrid.head()
